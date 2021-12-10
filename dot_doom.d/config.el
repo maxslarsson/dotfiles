c061294ef780664ab19ef2019bab7ce45d3c38f4
Max Larsson <maxslarsson@gmail.com>
2021-12-10 15:00:58 -0500

    Update dot_config/private_fish/fish_variables Update dot_doom.d/config.el Update dot_doom.d/init.el

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Max Larsson"
+      user-mail-address "maxslarsson@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -33,8 +33,9 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
+;(setq +latex-viewers '(pdf-tools latex-preview-pane skim.app evince sumatrapdf zathura okular))
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
