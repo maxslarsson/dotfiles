2f03766b7ad3caae22c35ffa8ff82aa2bcf40db6
Max Larsson <maxslarsson@gmail.com>
2023-07-05 17:09:23 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [[ -z $STOW_FOLDERS ]]; then
+    STOW_FOLDERS="bin,nvim,tmux,zsh"
+fi
+
+if [[ -z $DOTFILES ]]; then
+    DOTFILES=$HOME/dev/dotfiles
+fi
+
+pushd $DOTFILES
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do
+    echo "stow $folder"
+    stow -D $folder -t $HOME
+    stow $folder -t $HOME
+done
+popd
+
